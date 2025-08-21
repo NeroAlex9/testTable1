51aeea3017088135171874ce9f4c14aba76d8d60
NeroAlex9 <Nerobelov1997@mail.ru>
2025-08-21 15:57:32 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/testTable/',
+  base: '/testTable1/',
   build: {
     outDir: 'build' // теперь сборка будет в папку build
   }
